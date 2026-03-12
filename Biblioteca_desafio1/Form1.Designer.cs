c8f2876c6e4b8fe57c34e50110afcd57fdf3ca94
hectorjavier1925-afk <hectorjavier1925@gmail.com>
2026-03-11 23:26:27 -0600

    Diseño de interfaz terminado y nombres de controles configurados

--- Biblioteca_desafio1/Form1.Designer.cs
+++ Biblioteca_desafio1/Form1.Designer.cs
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LblTitulo = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.lblAutor = new System.Windows.Forms.Label();
+            this.LblISBN = new System.Windows.Forms.Label();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.dgvInventario = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventario)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LblTitulo
+            // 
+            this.LblTitulo.AutoSize = true;
+            this.LblTitulo.Location = new System.Drawing.Point(57, 30);
+            this.LblTitulo.Name = "LblTitulo";
+            this.LblTitulo.Size = new System.Drawing.Size(43, 16);
+            this.LblTitulo.TabIndex = 0;
+            this.LblTitulo.Text = "Titulo:";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(33, 49);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(100, 22);
+            this.txtTitulo.TabIndex = 1;
+            // 
+            // lblAutor
+            // 
+            this.lblAutor.AutoSize = true;
+            this.lblAutor.Location = new System.Drawing.Point(235, 30);
+            this.lblAutor.Name = "lblAutor";
+            this.lblAutor.Size = new System.Drawing.Size(41, 16);
+            this.lblAutor.TabIndex = 2;
+            this.lblAutor.Text = "Autor:";
+            this.lblAutor.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // LblISBN
+            // 
+            this.LblISBN.AutoSize = true;
+            this.LblISBN.Location = new System.Drawing.Point(392, 30);
+            this.LblISBN.Name = "LblISBN";
+            this.LblISBN.Size = new System.Drawing.Size(38, 16);
+            this.LblISBN.TabIndex = 3;
+            this.LblISBN.Text = "ISBN";
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(200, 49);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(100, 22);
+            this.txtAutor.TabIndex = 4;
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Location = new System.Drawing.Point(355, 49);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(100, 22);
+            this.txtISBN.TabIndex = 5;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(566, 48);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(155, 23);
+            this.btnRegistrar.TabIndex = 6;
+            this.btnRegistrar.Text = "Registrar Libro";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // dgvInventario
+            // 
+            this.dgvInventario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInventario.Location = new System.Drawing.Point(285, 239);
+            this.dgvInventario.Name = "dgvInventario";
+            this.dgvInventario.RowHeadersWidth = 51;
+            this.dgvInventario.RowTemplate.Height = 24;
+            this.dgvInventario.Size = new System.Drawing.Size(240, 150);
+            this.dgvInventario.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvInventario);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.txtISBN);
+            this.Controls.Add(this.txtAutor);
+            this.Controls.Add(this.LblISBN);
+            this.Controls.Add(this.lblAutor);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.LblTitulo);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventario)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label LblTitulo;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.Label lblAutor;
+        private System.Windows.Forms.Label LblISBN;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.DataGridView dgvInventario;
     }
 }
 
--- Biblioteca_desafio1/Form1.cs
+++ Biblioteca_desafio1/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Biblioteca_Desafio1;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,38 @@ namespace Biblioteca_desafio1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Creamos el libro con lo que escribiste en los cuadros
+                Libro nuevo = new Libro(txtTitulo.Text, txtAutor.Text, txtISBN.Text, "General");
+
+                // Lo guardamos en el cerebro de la biblioteca
+                // (Asegurate de tener creada la variable miBiblioteca arriba)
+                MessageBox.Show("¡Libro registrado, chucha! Ya estuvo.", "Éxito");
+
+                // Limpiamos los cuadros para el siguiente
+                txtTitulo.Clear();
+                txtAutor.Clear();
+                txtISBN.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Valió verga: " + ex.Message);
+            }
         }
     }
 }
